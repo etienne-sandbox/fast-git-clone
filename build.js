261e1efe246307cf0cc2adaefe4b397bab488edd
Etienne Deladonchamps <e.deladonchamps@gmail.com>
2022-02-06 12:41:41 +0100

    Move to esm

--- build.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env node
-const { build } = require("estrella");
-
-build({
-  entry: "src/index.js",
-  outfile: "build/main.js",
-  format: "cjs",
-});
--- src/index.js
+++ src/index.js
@@ -1,44 +1,47 @@
-import inquirer from 'inquirer';
-import gitPullOrClone from 'git-pull-or-clone';
-import path from 'path';
-import os from 'os';
+#!/usr/bin/env node
+import inquirer from "inquirer";
+import gitPullOrClone from "git-pull-or-clone";
+import * as path from "node:path";
+import * as os from "node:os";
+
+try {
+  await main();
+} catch (error) {
+  console.error(err);
+  process.exit(1);
+}
 
 async function main() {
   const HOMEDIR = os.homedir();
-  const GITHUBDIR = path.resolve(HOMEDIR, 'github');
+  const GITHUBDIR = path.resolve(HOMEDIR, "github");
 
   const res = await inquirer.prompt({
-    type: 'input',
-    name: 'repo',
+    type: "input",
+    name: "repo",
     message: `Enter the user/repo`,
     validate: (val) => {
-      const parts = val.split('/');
+      const parts = val.split("/");
       if (parts.length === 2) {
         return true;
       }
       if (parts.length === 1) {
-        return 'Missing repo name !';
+        return "Missing repo name !";
       }
       return `Invalis string`;
     },
   });
 
-  const [user, repo] = res.repo.split('/');
+  const [user, repo] = res.repo.split("/");
 
   const url = `git@github.com:${user}/${repo}.git`;
   const repoPath = path.resolve(GITHUBDIR, user, repo);
 
-  console.log(`Clonning...`);
+  console.log(`Clonning ${url}\n    into ${repoPath}`);
   await gitPullOrCloneAsync(url, repoPath);
   console.log(`Repo cloned in ${repoPath}`);
 
   process.exit();
 }
-
-main().catch((err) => {
-  console.error(err);
-  process.exit();
-});
 
 function gitPullOrCloneAsync(url, path) {
   return new Promise((resolve, reject) => {
